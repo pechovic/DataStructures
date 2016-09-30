915f3b38dd2dbda9454e56f6ffb4c8cb9b1cd4ec
Petr P <csnetsource@users.noreply.github.com>
2016-09-30 21:30:26 +0200

    AVL tree node implemented

--- DataStructures/DataStructures.Test/AvlTreeNodeTest.cs
+++ DataStructures/DataStructures.Test/AvlTreeNodeTest.cs
@@ -11,6 +11,58 @@ namespace DataStructures.Test
     public class AvlTreeNodeTest
     {
         [TestMethod]
+        public void Node_Less_Then_Test()
+        {
+            var node1 = new AvlTreeNode<int>(7);
+            var node2 = new AvlTreeNode<int>(8);
+            var node3 = new AvlTreeNode<int>(8);
+
+            Assert.IsTrue(node1 < node2);
+            Assert.IsFalse(node2 < node1);
+            Assert.IsFalse(node2 < node3);
+            Assert.IsFalse(node3 < node2);
+        }
+
+        [TestMethod]
+        public void Node_Greater_Then_Test()
+        {
+            var node1 = new AvlTreeNode<int>(7);
+            var node2 = new AvlTreeNode<int>(8);
+            var node3 = new AvlTreeNode<int>(8);
+
+            Assert.IsFalse(node1 > node2);
+            Assert.IsTrue(node2 > node1);
+            Assert.IsFalse(node2 > node3);
+            Assert.IsFalse(node3 > node2);
+        }
+
+        [TestMethod]
+        public void Node_Equal_Test()
+        {
+            var node1 = new AvlTreeNode<int>(7);
+            var node2 = new AvlTreeNode<int>(8);
+            var node3 = new AvlTreeNode<int>(8);
+
+            Assert.IsFalse(node1 == node2);
+            Assert.IsFalse(node2 == node1);
+            Assert.IsTrue(node2 == node3);
+            Assert.IsTrue(node3 == node2);
+        }
+
+        [TestMethod]
+        public void Node_NotEqual_Test()
+        {
+            var node1 = new AvlTreeNode<int>(7);
+            var node2 = new AvlTreeNode<int>(8);
+            var node3 = new AvlTreeNode<int>(8);
+
+            Assert.IsTrue(node1 != node2);
+            Assert.IsTrue(node2 != node1);
+            Assert.IsFalse(node2 != node3);
+            Assert.IsFalse(node3 != node2);
+        }
+
+        [TestMethod]
         public void Add_RootNode_Ok()
         {
             var node = new AvlTreeNode<int>(1);
@@ -24,7 +76,7 @@ namespace DataStructures.Test
         public void Insert_Balance_0()
         {
             var rootNode = Get12345Tree();
-            Assert.AreEqual(0, rootNode.BalanceFactor);
+            Assert.AreEqual(TreeState.Balanced, rootNode.State);
         }
 
         [TestMethod]
@@ -33,26 +85,78 @@ namespace DataStructures.Test
             var rootNode = Get12345Tree();
             Assert.AreEqual(2, rootNode.Height);
         }
-
+        
         [TestMethod]
-        public void Add_InOrderTraversal_Sorted_Order()
+        public void RightRotation_Ok()
         {
-            var rootNode = Get12345Tree();
-
-            StringBuilder sb = new StringBuilder();
-            rootNode.InOrderTraversal(n => { sb.Append(n.Value.ToString()); });
-
-            Assert.AreEqual("12345", sb.ToString());
+            // on the following tree the right rotation will be performed
+            var node = new AvlTreeNode<int>(5);
+            node.Add(new AvlTreeNode<int>(4));
+            node.Add(new AvlTreeNode<int>(3));
+            
+            Assert.AreEqual(new AvlTreeNode<int>(4), node.GetRoot());
+            Assert.AreEqual(new AvlTreeNode<int>(3), node.GetRoot().LeftNode);
+            Assert.AreEqual(new AvlTreeNode<int>(5), node.GetRoot().RightNode);
+            Assert.AreEqual(1, node.GetRoot().Height);
         }
 
+        [TestMethod]
+        public void LeftRotation_Ok()
+        {
+            // on the following tree the right rotation will be performed
+            var node = new AvlTreeNode<int>(1);
+            node.Add(new AvlTreeNode<int>(2));
+            node.Add(new AvlTreeNode<int>(3));
 
-        private Node<int> Get12345Tree()
+            Assert.AreEqual(new AvlTreeNode<int>(2), node.GetRoot());
+            Assert.AreEqual(new AvlTreeNode<int>(1), node.GetRoot().LeftNode);
+            Assert.AreEqual(new AvlTreeNode<int>(3), node.GetRoot().RightNode);
+            Assert.AreEqual(1, node.GetRoot().Height);
+        }
+
+        [TestMethod]
+        public void Add_Balance_Height_Find_Ok()
+        {
+            var rootNode = GetBigTree();
+            
+            Assert.AreEqual(13, rootNode.Height);
+            Assert.IsNotNull(rootNode.Find(9));
+            Assert.IsNotNull(rootNode.Find(998));
+            Assert.IsNotNull(rootNode.Find(345));
+            Assert.IsNotNull(rootNode.Find(6789));
+        }
+
+        [TestMethod]
+        public void RightLeftRotation_Ok()
+        {
+            var node = new AvlTreeNode<int>(3);
+            node.GetRoot().Add(new AvlTreeNode<int>(1));
+            node.GetRoot().Add(new AvlTreeNode<int>(2));
+
+            Assert.AreEqual(new AvlTreeNode<int>(2), node.GetRoot());
+            Assert.AreEqual(new AvlTreeNode<int>(1), node.GetRoot().LeftNode);
+            Assert.AreEqual(new AvlTreeNode<int>(3), node.GetRoot().RightNode);
+            Assert.AreEqual(1, node.GetRoot().Height);
+        }
+
+        private AvlTreeNode<int> Get12345Tree()
         {
             var tree = new AvlTreeNode<int>(1);
-            tree.Add(new AvlTreeNode<int>(2));
-            tree.Add(new AvlTreeNode<int>(3));
-            tree.Add(new AvlTreeNode<int>(4));
-            tree.Add(new AvlTreeNode<int>(5));
+            tree.GetRoot().Add(new AvlTreeNode<int>(2));
+            tree.GetRoot().Add(new AvlTreeNode<int>(3));
+            tree.GetRoot().Add(new AvlTreeNode<int>(4));
+            tree.GetRoot().Add(new AvlTreeNode<int>(5));
+
+            return tree.GetRoot();
+        }
+
+        private AvlTreeNode<int> GetBigTree()
+        {
+            var tree = new AvlTreeNode<int>(1);
+            for (int i = 2; i < 10000; i++)
+            {
+                tree.GetRoot().Add(new AvlTreeNode<int>(i));
+            }
 
             return tree.GetRoot();
         }
--- DataStructures/DataStructures/AvlTreeNode.cs
+++ DataStructures/DataStructures/AvlTreeNode.cs
@@ -1,22 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructures
 {
-    public class AvlTreeNode<T> : Node<T>
+    public class AvlTreeNode<T>
         where T : IComparable<T>
     {
-        
-        public override T Value { get; protected set; }
+        /// <summary>
+        /// Indicates when balancing will occur.
+        /// </summary>
+        private const int _balancingThreshold = 1;
 
-        private Node<T> _leftNode;
-        public override Node<T> LeftNode
+        public virtual int BalanceFactor
         {
             get
             {
+                return RightNode.Height - LeftNode.Height;
+            }
+        }
+
+        public virtual AvlTreeNode<T> Parent { get; internal set; }
+
+        public virtual TreeState State
+        {
+            get
+            {
+                if (BalanceFactor > _balancingThreshold)
+                {
+                    return TreeState.RightHeavy;
+                }
+                else if ((BalanceFactor * -1) > _balancingThreshold)
+                {
+                    return TreeState.LeftHeavy;
+                }
+
+                return TreeState.Balanced;
+            }
+        }
+
+        /// <summary>
+        /// Identifies whether this node is the root node or not.
+        /// </summary>
+        public bool IsRootNode
+        {
+            get
+            {
+                return Parent == null;
+            }
+        }
+
+        public T Value { get; protected set; }
+        
+        private AvlTreeNode<T> _leftNode;
+        public AvlTreeNode<T> LeftNode
+        {
+            get
+            {
+                if (_leftNode == null)
+                {
+                    return new EmptyLeaf();
+                }
+
                 return _leftNode;
             }
             internal set
@@ -26,11 +69,16 @@ namespace DataStructures
             }
         }
 
-        private Node<T> _rightNode;
-        public override Node<T> RightNode
+        private AvlTreeNode<T> _rightNode;
+        public AvlTreeNode<T> RightNode
         {
             get
             {
+                if (_rightNode == null)
+                {
+                    return new EmptyLeaf();
+                }
+
                 return _rightNode;
             }
             internal set
@@ -40,12 +88,10 @@ namespace DataStructures
             }
         }
 
-        public override Node<T> Parent { get; internal set; }
-
         /// <summary>
         /// Classic hight of the node.
         /// </summary>
-        public override int Height
+        public virtual int Height
         {
             get
             {
@@ -56,52 +102,337 @@ namespace DataStructures
             }
         }
 
-        public override int BalanceFactor
-        {
-            get
-            {
-                return LeftNode.Height - RightNode.Height;
-            }
-        }
-
         public AvlTreeNode()
         {
-            InitializeChildNodes();
         }
 
         public AvlTreeNode(T value)
         {
             Value = value;
-            InitializeChildNodes();
         }
 
-        private void InitializeChildNodes()
+        public AvlTreeNode<T> GetRoot()
         {
-            LeftNode = new EmptyNode<T>();
-            RightNode = new EmptyNode<T>();
+            if (IsRootNode)
+            {
+                return this;
+            }
+
+            return Parent.GetRoot();
         }
+       
+        #region Add/Remove/Find
 
-        #region Insert/Delete
-
-        public override Node<T> Add(Node<T> node)
+        public void Add(AvlTreeNode<T> node)
         {
             if (this > node)
             {
-                LeftNode = LeftNode.Add(node);
+                if (LeftNode is EmptyLeaf)
+                {
+                    LeftNode = node;
+                    Balance();
+                    return;
+                }
+
+                LeftNode.Add(node);
             }
             else
             {
-                RightNode = RightNode.Add(node);
-            }
+                if (RightNode is EmptyLeaf)
+                {
+                    RightNode = node;
+                    Balance();
+                    return;
+                }
 
-            return this;
+                RightNode.Add(node);
+            }
         }
 
-        public override void Remove()
+        public void Remove()
         {
+            throw new NotImplementedException();
+        }
 
+        public AvlTreeNode<T> Find(T value)
+        {
+            if (this is EmptyLeaf)
+            {
+                return null;
+            }
+
+            if (Value.CompareTo(value) == 0)
+            {
+                return this;
+            }
+
+            if (Value.CompareTo(value) > 0)
+            {
+                return LeftNode.Find(value);
+            }
+
+            return RightNode.Find(value);
         }
 
         #endregion
+
+        #region Balancing
+
+        /// <summary>
+        /// Decide what rotation to take on this tree and perform.
+        /// </summary>
+        public void Balance()
+        {
+            if (State == TreeState.RightHeavy)
+            {
+                if (RightNode.BalanceFactor < 0)
+                {
+                    LeftRightRotation();
+                }
+                else
+                {
+                    LeftRotation();
+                }
+            }
+            else if (State == TreeState.LeftHeavy)
+            {
+                if (LeftNode.BalanceFactor > 0)
+                {
+                    RightLeftRotation();
+                }
+                else
+                {
+                    RightRotation();
+                }
+            }
+
+            if (!IsRootNode)
+            {
+                Parent.Balance();
+            }
+        }
+
+        private void LeftRotation()
+        {
+            var newLocalRoot = RightNode;
+            ReplaceRoot(newLocalRoot);
+            
+            RightNode = newLocalRoot.LeftNode;
+
+            newLocalRoot.LeftNode = this;
+        }
+
+        private void RightRotation()
+        {
+            var newLocalRoot = LeftNode;
+            ReplaceRoot(newLocalRoot);
+            
+            LeftNode = newLocalRoot.RightNode;
+            newLocalRoot.RightNode = this;
+        }
+
+        private void ReplaceRoot(AvlTreeNode<T> newRoot)
+        {
+            // if this isn't a root node, it has a parent
+            if (!IsRootNode)
+            {
+                // determine which child of the Parent must be replaced
+                if (Parent.LeftNode == this)
+                {
+                    Parent.LeftNode = newRoot;
+                }
+                else if (Parent.RightNode == this)
+                {
+                    Parent.RightNode = newRoot;
+                }
+                else
+                {
+                    throw new Exception("A problem with an algorithm?");
+                }
+            }
+            else
+            {
+                // make it root of the whole tree
+                newRoot.Parent = null;
+            }
+        }
+
+
+        private void LeftRightRotation()
+        {
+            RightNode.RightRotation();
+            LeftRotation();
+        }
+
+        private void RightLeftRotation()
+        {
+            LeftNode.LeftRotation();
+            RightRotation();
+        }
+
+        #endregion
+
+        #region Traversal
+
+        public virtual void InOrderTraversal(Action<AvlTreeNode<T>> actionOnNode)
+        {
+            LeftNode.InOrderTraversal(actionOnNode);
+            actionOnNode(this);
+
+            if (RightNode != null)
+            {
+                RightNode.InOrderTraversal(actionOnNode);
+            }
+        }
+
+        public virtual void PreOrderTraversal(Action<AvlTreeNode<T>> actionOnNode)
+        {
+            actionOnNode(this);
+            LeftNode.PreOrderTraversal(actionOnNode);
+            RightNode.PreOrderTraversal(actionOnNode);
+        }
+
+        public virtual void PostOrderTraversal(Action<AvlTreeNode<T>> actionOnNode)
+        {
+            LeftNode.PostOrderTraversal(actionOnNode);
+            RightNode.PostOrderTraversal(actionOnNode);
+            actionOnNode(this);
+        }
+
+        #endregion
+
+        #region Equality
+
+        public static bool operator <(AvlTreeNode<T> a, AvlTreeNode<T> b)
+        {
+            return a.Value.CompareTo(b.Value) < 0;
+        }
+
+        public static bool operator >(AvlTreeNode<T> a, AvlTreeNode<T> b)
+        {
+            return a.Value.CompareTo(b.Value) > 0;
+        }
+
+        public static bool operator ==(AvlTreeNode<T> a, AvlTreeNode<T> b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if (((object)a == null) || ((object)b == null))
+            {
+                return false;
+            }
+
+            return a.Value.CompareTo(b.Value) == 0;
+        }
+
+        public static bool operator !=(AvlTreeNode<T> a, AvlTreeNode<T> b)
+        {
+            return !(a == b);
+        }
+
+
+        /// <summary>
+        /// Defining value identity.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            AvlTreeNode<T> node = obj as AvlTreeNode<T>;
+            if (node == null)
+            {
+                return false;
+            }
+
+            return Equals(node);
+        }
+
+        /// <summary>
+        /// Defininig value identity
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public bool Equals(AvlTreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            return Value.CompareTo(node.Value) == 0;
+        }
+
+        /// <summary>
+        /// Identity drived by value. 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        #region EmptyLeaf
+
+        /// <summary>
+        /// Null object pattern for simple writing.
+        /// </summary>
+        private class EmptyLeaf : AvlTreeNode<T>
+        {
+            public override int BalanceFactor
+            {
+                get
+                {
+                    return 0;
+                }
+            }
+
+            public override int Height
+            {
+                get
+                {
+                    return -1;
+                }
+            }
+
+            public override void InOrderTraversal(Action<AvlTreeNode<T>> actionOnNode)
+            {
+            }
+
+            public override void PreOrderTraversal(Action<AvlTreeNode<T>> actionOnNode)
+            {
+            }
+
+            public override void PostOrderTraversal(Action<AvlTreeNode<T>> actionOnNode)
+            {
+            }
+        }
+
+        #endregion
+
     }
+
+    #region TreeState
+    public enum TreeState
+    {
+        Balanced,
+        LeftHeavy,
+        RightHeavy
+    }
+
+    #endregion
 }
