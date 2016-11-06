8cee08b9e8a7210668e7c6074c01d81ff480c2e8
Petr <csnetsource@users.noreply.github.com>
2016-11-06 21:36:00 +0100

    Completely changed the type of the project to be in Visual Studio Code

---
+++ DataStructures/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DataStructures {
+
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Reference this library and use it wisely :)");
+        }
+    }
+}
